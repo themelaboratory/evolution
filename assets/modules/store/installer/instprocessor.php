e077d2023998d076e0c7efaee194f4f577897342
Dmi3yy <dmi3yy@gmail.com>
2020-08-10 00:15:13 +0300

    revert (fix lost plugin events on install) - no need becouse already done

--- assets/modules/store/installer/instprocessor.php
+++ assets/modules/store/installer/instprocessor.php
@@ -376,14 +376,14 @@ if (isset ($_POST['plugin']) || $installData) {
                                 LEFT JOIN `{$table_prefix}site_plugin_events` `spe` ON `spe`.`evtid` = `se`.`id` AND `spe`.`pluginid` = {$prev_id}
                                 LEFT JOIN `{$table_prefix}site_plugin_events` `spe2` ON `spe2`.`evtid` = `se`.`id`
                                 WHERE name IN ('{$_events}')
-                                GROUP BY `se`.`id`, `priority`
+                                GROUP BY `se`.`id`
                             ");
                         } else {
                             $modx->db->query("INSERT IGNORE INTO `{$table_prefix}site_plugin_events` (`pluginid`, `evtid`, `priority`) 
                                 SELECT {$id} as `pluginid`, `se`.`id` as `evtid`, COALESCE(MAX(`spe`.`priority`) + 1, 0) as `priority` 
                                 FROM `{$table_prefix}system_eventnames` `se` 
                                 LEFT JOIN `{$table_prefix}site_plugin_events` `spe` ON `spe`.`evtid` = `se`.`id` 
-                                WHERE `name` IN ('{$_events}') GROUP BY `se`.`id`, `priority`
+                                WHERE `name` IN ('{$_events}') GROUP BY `se`.`id`
                             ");
                         }
 
