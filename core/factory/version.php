059c417632c41196a222505c01ad41cbe1a3c7ab
Dmi3yy <dmi3yy@gmail.com>
2020-08-10 00:15:26 +0300

    fix lost plugin events on install

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version' => '2.0.2', // Current version number
-    'release_date' => 'Mar 13, 2020', // Date of release
+    'version' => '2.0.3', // Current version number
+    'release_date' => 'Aug 10, 2020', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
-    'full_appname' => 'Evolution CMS 2.0.2 (Mar 13, 2020)'
+    'full_appname' => 'Evolution CMS 2.0.3 (Aug 10, 2020)'
 ];
